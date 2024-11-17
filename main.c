33c3a43145b7e8be7afe13568699e23d8bfbb3f3
JikMF2007 <1852248160@qq.com>
2024-11-17 19:42:11 +0800

    second commit

--- main.c
+++ main.c
@@ -1,1 +1,24 @@
 //请在下方输入你的代码：
+#include <stdio.h>
+
+int main() {
+    // 正三角
+    printf("正三角九九乘法表：\n");
+    for (int i = 1; i <= 9; i++)
+    {
+        for (int j = 1; j <= i; j++)
+            printf("%d×%d=%-2d ", j, i, i * j);
+        printf("\n");
+    }
+
+    // 倒三角
+    printf("\n倒三角九九乘法表：\n");
+    for (int i = 9; i >= 1; i--)
+    {
+        for (int j = 1; j <= i; j++)
+            printf("%d×%d=%-2d ", j, i, i * j);
+        printf("\n");
+    }
+
+    return 0;
+}
